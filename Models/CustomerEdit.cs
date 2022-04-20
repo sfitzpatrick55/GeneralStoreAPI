b82407cbcb334cdfd55ef7469d1160b8bb1c8916
Scott <sfitzpatrick55@gmail.com>
2022-04-20 17:29:34 -0400

    Build classes

---
+++ Models/CustomerEdit.cs
@@ -0,0 +1,9 @@
+
+namespace GeneralStoreAPI.Models
+{
+    public class CustomerEdit
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Models/ProductEdit.cs
@@ -0,0 +1,10 @@
+
+namespace GeneralStoreAPI.Models
+{
+    public class ProductEdit
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
